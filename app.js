5734ab7ff4fb2c49320877f2ed30f4642c1ebb6e
Kyle <37756806+kjm1550@users.noreply.github.com>
2019-05-25 19:50:03 -0500

    Added Google Analytics
    Added Google Analytics. Ran prettier on the code to make it cleamer and easier to read.

--- app.js
+++ app.js
@@ -2,23 +2,21 @@ const celciusInput = document.querySelector('#celcius > input');
 const fahrenheitInput = document.querySelector('#fahrenheit > input');
 const kelvinInput = document.querySelector('#kelvin > input');
 
-
-
 main();
 
-function main(){
+function main() {
 	celciusInput.addEventListener('input', celciusConvertRest);
 	fahrenheitInput.addEventListener('input', fahrenheitConvertRest);
 	kelvinInput.addEventListener('input', kelvinConverRest);
 }
 
 function roundNumber(num) {
-	return Math.round(num*100)/100;
+	return Math.round(num * 100) / 100;
 }
 
 function celciusConvertRest() {
 	const cTemp = parseFloat(celciusInput.value);
-	const fTemp = (cTemp * (9/5)) + 32;
+	const fTemp = cTemp * (9 / 5) + 32;
 	const kTemp = cTemp + 273.15;
 
 	fahrenheitInput.value = roundNumber(fTemp);
@@ -27,8 +25,8 @@ function celciusConvertRest() {
 
 function fahrenheitConvertRest() {
 	const fTemp = parseFloat(fahrenheitInput.value);
-	const cTemp = (fTemp - 32) * (5/9);
-	const kTemp = (fTemp + 459.67) * (5/9);
+	const cTemp = (fTemp - 32) * (5 / 9);
+	const kTemp = (fTemp + 459.67) * (5 / 9);
 
 	celciusInput.value = roundNumber(cTemp);
 	kelvinInput.value = roundNumber(kTemp);
@@ -36,8 +34,8 @@ function fahrenheitConvertRest() {
 
 function kelvinConverRest() {
 	const kTemp = parseFloat(kelvinInput.value);
-	const cTemp = (kTemp - 273.15);
-	const fTemp = (9/5) * (kTemp - 273) + 32;
+	const cTemp = kTemp - 273.15;
+	const fTemp = (9 / 5) * (kTemp - 273) + 32;
 
 	celciusInput.value = roundNumber(cTemp);
 	fahrenheitInput.value = roundNumber(fTemp);
